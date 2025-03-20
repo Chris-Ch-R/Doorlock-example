ac3f9ee40d6572730b32d37db12e5b680c8bbc88
Chris-Ch-R <chris@chayangkuns-Mac-mini.local>
2025-03-20 18:14:01 +0700

    correct command send and refactor code

--- lib/salto_service.dart
+++ lib/salto_service.dart
@@ -4,16 +4,20 @@ class ServiceSalto {
   Future<void> initializeSalto() async {
     try {
       // Socket socket = await Socket.connect('0.0.0.0', 8888);
-      Socket socket = await Socket.connect('192.168.1.49', 8888,timeout: const Duration(seconds: 5));
+      Socket socket = await Socket.connect('192.168.1.49', 8888,
+          timeout: const Duration(seconds: 5));
       // Socket socket = await Socket.connect('10.0.0.2', 8888);
-      print('Connected to: ${socket.remoteAddress.address}:${socket.remotePort}');
+      print(
+          'Connected to: ${socket.remoteAddress.address}:${socket.remotePort}');
 
       // Send data
-      socket.write('CNMB|+66867866564|101|');
-      print("socket.write : CNMB|+66867866564|101");
+      socket.writeln('\x02CNMB|+66867866564|101\x03');
+      print("socket.write : \x02CNMB|+66867866564|101\x03");
       // Listen for incoming data
       socket.listen(
-            (data) {
+        (data) {
+          String s = new String.fromCharCodes(data);
+          print("data : ${data}");
           print('Received: ${String.fromCharCodes(data)}');
         },
         onDone: () {
